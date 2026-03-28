b0562122b37ac906e6ecfb819c48e921b3fdf661
GGBoss <434207278@qq.com>
2026-03-28 18:09:21 +0800

    改用Katex公式支持并更新密码学文档

---
+++ docs/assets/javascripts/katex-init.js
@@ -0,0 +1,17 @@
+function renderMath() {
+  if (typeof renderMathInElement !== 'undefined') {
+    renderMathInElement(document.body, {
+      delimiters: [
+        {left: '$$', right: '$$', display: true},
+        {left: '$', right: '$', display: false},
+        {left: '\\(', right: '\\)', display: false},
+        {left: '\\[', right: '\\]', display: true}
+      ],
+      throwOnError: false
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", renderMath);
+
+document$.subscribe(renderMath);
